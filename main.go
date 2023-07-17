7bdfe83f810349997a3b9bd44f2759f3f876f23e
Daniel Rastelli <rastellidani@gmail.com>
2023-07-17 10:09:23 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var queues = []string{"queue_a", "queue_b", "queue_c", "queue_d", "queue_e", "queue_f"}
+
+func generateData() map[string]float64 {
+	data := make(map[string]float64)
+
+	for _, queue := range queues {
+		// Chance to generate
+		if rand.Intn(2)%2 != 0 {
+			data[queue] = rand.Float64()
+		}
+	}
+
+	return data
+}
+
+func main() {
+	// Create a non-global registry.
+	reg := prometheus.NewRegistry()
+
+	vec := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "testMetric",
+			Help: "A not very useful metric",
+		},
+		[]string{"queue"},
+	)
+
+	reg.MustRegister(vec)
+
+	go func() {
+		for true {
+			data := generateData()
+			vec.Reset()
+
+			for queue, val := range data {
+				vec.WithLabelValues(queue).Set(val)
+			}
+
+			fmt.Println("Data regenerated")
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	// Expose metrics and custom registry via an HTTP server
+	// using the HandleFor function. "/metrics" is the usual endpoint for that.
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
